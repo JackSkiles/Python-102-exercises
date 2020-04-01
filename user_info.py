53e64e8a9fc8f01870589b9e805c0033e82ac1d8
Jack Skiles <jackskiles420@gmail.com>
2020-04-01 15:46:10 -0400

    Some updates

--- user_info.py
+++ user_info.py
@@ -1,6 +1,7 @@
 
 #Takes user input and splits it into three parts seperated by a comma and puts them into a list
-info = input('Enter these three things seperated by commas. What was your first pets name? what street did you grow up on? An ajective').split(',')
+info = input('''Enter these three things seperated by commas. What was your first pets name? 
+what street did you grow up on? An ajective''').split(',')
 
 #prints out the list with those three parts in specified order
 print('Hello, the ' + info[2] + ' ' + info[0] + ' ' + info[1])
