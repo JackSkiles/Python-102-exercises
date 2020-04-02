f2cbcba6213ff3312ff2d062a0d5db60f0e4b227
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 09:26:34 -0400

    Removed some comments

--- number_game.py
+++ number_game.py
@@ -4,8 +4,7 @@ import random
 #creates random variable that picks a number between 0 and 10
 rand1 = random.randint(0 , 10)
 
-#guess = input('Please input a guess number between 0 and 10 ')
-#guess = int(guess)
+
 
 #creates a for loop that iterates 5 times and asks the user for input up to five times. If the user gets it correct the program breaks and congradualtes you.
 for i in range(5):
